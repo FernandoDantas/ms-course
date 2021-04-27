0b3ac349e1925db3fee62257e3e274002a29c6df
Fernando <fernandojrsud@hotmail.com>
2021-04-27 19:07:07 -0300

    User, Role, many to many association

---
+++ hr-user/src/main/java/br/com/fernandojr/hruser/entities/Role.java
@@ -0,0 +1,86 @@
+package br.com.fernandojr.hruser.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_role")
+public class Role implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;	
+	private String roleName;
+
+	public Role() {			
+	}
+
+	public Role(Long id, String roleName) {
+		super();
+		this.id = id;
+		this.roleName = roleName;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the roleName
+	 */
+	public String getRoleName() {
+		return roleName;
+	}
+
+	/**
+	 * @param roleName the roleName to set
+	 */
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((roleName == null) ? 0 : roleName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof Role)) {
+			return false;
+		}
+		Role other = (Role) obj;
+		if (roleName == null) {
+			if (other.roleName != null) {
+				return false;
+			}
+		} else if (!roleName.equals(other.roleName)) {
+			return false;
+		}
+		return true;
+	}	
+	
+	
+}
